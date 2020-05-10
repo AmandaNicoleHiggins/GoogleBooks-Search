352d68a45317c0bee889501501f99774bc89be3d
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-05-10 19:02:04 -0400

    adding files

---
+++ build/precache-manifest.846bdad8495a92f69d897f898b147e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41d80ce884bdc5fc885781a6b9fe47a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d28e167db918903b857b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9a863c8b8de4b7e81a8",
+    "url": "/static/js/2.4f5be06a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f5be06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28e167db918903b857b",
+    "url": "/static/js/main.77da5a4d.chunk.js"
+  },
+  {
+    "revision": "934616984ed328925a35",
+    "url": "/static/js/runtime-main.38648221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
